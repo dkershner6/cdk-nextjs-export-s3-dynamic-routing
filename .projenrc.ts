01bf8c52ae0a722c32ae81c8a806d25899595a81
Derek Kershner <kershner@eformedpartners.com>
2023-04-29 09:37:47 -0700

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Derek Kershner',
+  authorAddress: 'kershner@eformedpartners.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: 'cdk-nextjs-export-s3-dynamic-routing',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/dkershner6/cdk-nextjs-export-s3-dynamic-routing.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# cdk-nextjs-export-s3-dynamic-routing
-Deploy a static export Next.js site to Cloudfront and S3 while maintaining the ability to use dynamic routes
+# replace this
