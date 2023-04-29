1e3c2ec6f7e5885e394f10afda5701462ca7aff6
Derek Kershner <kershner@eformedpartners.com>
2023-04-29 15:03:59 -0700

    uglify-js

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,11 +9,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
   projenrcTs: true,
   repositoryUrl: 'https://github.com/dkershner6/cdk-nextjs-export-s3-dynamic-routing.git',
 
-  deps: ['lodash.set'], /* Runtime dependencies of this module. */
+  deps: ['lodash.set', 'uglify-js'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  devDeps: ['@types/lodash.set'], /* Build dependencies for this module. */
+  devDeps: ['@types/lodash.set', '@types/uglify-js'], /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
-  bundledDeps: ['lodash.set'], /* Bundled dependencies of this module. */
+  bundledDeps: ['lodash.set', 'uglify-js'], /* Bundled dependencies of this module. */
 
   tsconfig: {
     compilerOptions: {
