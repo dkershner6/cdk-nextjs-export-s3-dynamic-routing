4ee3ae2020dcea5405a39207e74025822a644010
Derek Kershner <dkershner@dkershner.com>
2023-12-22 09:50:41 -0800

    pyPi added

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,7 @@
 import { awscdk } from 'projen';
+
+const PROJECT_NAME = 'cdk-nextjs-export-s3-dynamic-routing';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   majorVersion: 1,
   author: 'Derek Kershner',
@@ -6,16 +9,23 @@ const project = new awscdk.AwsCdkConstructLibrary({
   cdkVersion: '2.77.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
-  name: 'cdk-nextjs-export-s3-dynamic-routing',
+  name: PROJECT_NAME,
   projenrcTs: true,
   repositoryUrl: 'https://github.com/dkershner6/cdk-nextjs-export-s3-dynamic-routing.git',
 
   deps: ['lodash.set', 'uglify-js'], /* Runtime dependencies of this module. */
   description: 'Deploy a static export Next.js site to Cloudfront and S3 while maintaining the ability to use dynamic routes.', /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: ['@types/lodash.set', '@types/uglify-js'], /* Build dependencies for this module. */
-  packageName: 'cdk-nextjs-export-s3-dynamic-routing', /* The "name" in package.json. */
+  packageName: PROJECT_NAME, /* The "name" in package.json. */
   bundledDeps: ['lodash.set', 'uglify-js'], /* Bundled dependencies of this module. */
 
   gitignore: ['.DS_Store'],
+
+  // Publish to other languages
+  publishToPypi: {
+    distName: PROJECT_NAME,
+    module: PROJECT_NAME.replace('-', '_'),
+  },
 });
+
 project.synth();
