fc545ebcced828b6e26dc6647ea5f577de0761e7
Derek Kershner <dkershner@dkershner.com>
2023-12-25 20:42:07 -0800

    feat: fully updated with new construct

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,61 +1,63 @@
-import { Node20AwsCdkConstructLibrary } from "dkershner6-projen";
+import { Node20AwsCdkConstructLibrary } from 'dkershner6-projen';
 
-const GITHUB_USERNAME_OR_ORG = "dkershner6";
-const GITHUB_USERNAME_OR_ORG_PASCAL_CASE = "DKershner6";
-const PROJECT_NAME = "cdk-nextjs-export-s3-dynamic-routing";
-const PROJECT_NAME_PASCAL_CASE = "CdkNextjsExportS3DynamicRouting";
+
+const GITHUB_USERNAME_OR_ORG = 'dkershner6';
+const GITHUB_USERNAME_OR_ORG_PASCAL_CASE = 'DKershner6';
+const PROJECT_NAME = 'cdk-nextjs-export-s3-dynamic-routing';
+const PROJECT_NAME_PASCAL_CASE = 'CdkNextjsExportS3DynamicRouting';
 
 const project = new Node20AwsCdkConstructLibrary({
-    majorVersion: 1,
-    author: "Derek Kershner",
-    authorAddress: "https://dkershner.com",
-    cdkVersion: "2.77.0",
-    defaultReleaseBranch: "main",
-    jsiiVersion: "~5.0.0",
-    name: PROJECT_NAME,
-    projenrcTs: true,
-    repositoryUrl:
-        "https://github.com/dkershner6/cdk-nextjs-export-s3-dynamic-routing.git",
 
-    /* Runtime dependencies of this module. */
-    deps: ["lodash.set", "uglify-js"],
-    description:
-        "Deploy a static export Next.js site to Cloudfront and S3 while maintaining the ability to use dynamic routes." /* The description is just a string that helps people understand the purpose of the package. */,
-    devDeps: [
-        "@types/lodash.set",
-        "@types/uglify-js",
-        "dkershner6-projen",
-        "projen-nvm",
-    ] /* Build dependencies for this module. */,
-    packageName: PROJECT_NAME /* The "name" in package.json. */,
-    bundledDeps: [
-        "lodash.set",
-        "uglify-js",
-    ] /* Bundled dependencies of this module. */,
+  majorVersion: 1,
+  author: 'Derek Kershner',
+  authorAddress: 'https://dkershner.com',
+  cdkVersion: '2.77.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: PROJECT_NAME,
+  projenrcTs: true,
+  repositoryUrl:
+        'https://github.com/dkershner6/cdk-nextjs-export-s3-dynamic-routing.git',
 
-    gitignore: [".DS_Store"],
+  /* Runtime dependencies of this module. */
+  deps: ['lodash.set', 'uglify-js'],
+  description:
+        'Deploy a static export Next.js site to Cloudfront and S3 while maintaining the ability to use dynamic routes.' /* The description is just a string that helps people understand the purpose of the package. */,
+  devDeps: [
+    '@types/lodash.set',
+    '@types/uglify-js',
+    'dkershner6-projen',
+    'projen-nvm',
+  ] /* Build dependencies for this module. */,
+  packageName: PROJECT_NAME /* The "name" in package.json. */,
+  bundledDeps: [
+    'lodash.set',
+    'uglify-js',
+  ] /* Bundled dependencies of this module. */,
 
-    // Publish to other languages
-    publishToPypi: {
-        distName: PROJECT_NAME,
-        module: PROJECT_NAME.replace("-", "_"),
-    },
+  gitignore: ['.DS_Store'],
 
-    publishToNuget: {
-        packageId: `${GITHUB_USERNAME_OR_ORG_PASCAL_CASE}.${PROJECT_NAME_PASCAL_CASE}`,
-        dotNetNamespace: `${GITHUB_USERNAME_OR_ORG_PASCAL_CASE}.${PROJECT_NAME_PASCAL_CASE}`,
-    },
+  // Publish to other languages
+  publishToPypi: {
+    distName: PROJECT_NAME,
+    module: PROJECT_NAME.replace('-', '_'),
+  },
 
-    publishToGo: {
-        moduleName: `github.com/${GITHUB_USERNAME_OR_ORG}/${PROJECT_NAME}`,
-    },
+  publishToNuget: {
+    packageId: `${GITHUB_USERNAME_OR_ORG_PASCAL_CASE}.${PROJECT_NAME_PASCAL_CASE}`,
+    dotNetNamespace: `${GITHUB_USERNAME_OR_ORG_PASCAL_CASE}.${PROJECT_NAME_PASCAL_CASE}`,
+  },
 
-    // publishToMaven: {
-    //   mavenGroupId: `io.github.${GITHUB_USERNAME_OR_ORG}`,
-    //   javaPackage: `io.github.${GITHUB_USERNAME_OR_ORG}.${PROJECT_NAME.replace('-', '')}`,
-    //   mavenArtifactId: PROJECT_NAME,
-    //   mavenEndpoint: 'https://s01.oss.sonatype.org',
-    // },
+  publishToGo: {
+    moduleName: `github.com/${GITHUB_USERNAME_OR_ORG}/${PROJECT_NAME}`,
+  },
+
+  // publishToMaven: {
+  //   mavenGroupId: `io.github.${GITHUB_USERNAME_OR_ORG}`,
+  //   javaPackage: `io.github.${GITHUB_USERNAME_OR_ORG}.${PROJECT_NAME.replace('-', '')}`,
+  //   mavenArtifactId: PROJECT_NAME,
+  //   mavenEndpoint: 'https://s01.oss.sonatype.org',
+  // },
 });
 
 project.synth();
